be413f746c404c5f4aa4db5301c187636ecb9eef
puppyriver <4278246@qq.com>
2017-12-09 22:12:39 +0800

    11

--- stockholm/quota_agent.py
+++ stockholm/quota_agent.py
@@ -1,10 +1,26 @@
 import requests
+from day_info import  DayInfo
 class QuotaAgent :
-    url = "http://hq.sinajs.cn"
+    real_time_url = "http://hq.sinajs.cn"
+    history_url = "http://q.stock.sohu.com/hisHq"
     def fetchDayInfo(self,code):
         r_params = {'list': code}
-        r = requests.get(self.url, params=r_params)
-        print(r.json())
+        r = requests.get(self.real_time_url, params=r_params)
+        if (r.status_code == 200):
+            print(r.text)
+            text = r.text
+            text = text[ text.index("\"")+1:text.rindex("\"")]
+            values = text.split(",")
+            yesterday = float(values[2]);
+            now = float(values[3])
+            rate = 100 * (now - yesterday) / yesterday
+            day_info = DayInfo(code,float(values[1]),now,float(values[5]),float(values[4]),rate,int(values[8]))
+            # print(r.json())
+
+    def fetchHistory(self,code,start,end):
+        r_params = {'code' : code,'start': start,'end':end,'stat':1,'order':'D','period':'d','rt':'jsonp'}
+        r = requests.get(self.history_url, params=r_params)
+
 
 
 if __name__ == '__main__':
