da07f6f29112adf8b28dd24dd397fae8dadc9f84
puppyriver <4278246@qq.com>
2017-12-20 20:07:43 +0800

    12

---
+++ stockholm/toolbox/download_files.py
@@ -0,0 +1,22 @@
+import urllib.request
+import os
+import os.path
+
+path = 'urls/imagenet.synset.txt'
+file_object = open(path)
+dir = path[path.rindex("/")+1:path.rindex(".")]
+if not (os.path.exists(dir)):
+    os.mkdir(dir)
+
+try:
+  for line in file_object:
+    try:
+        line = line.strip()
+        file_name = line[line.rindex("/")+1:]
+        print('%s save to %s' % (line,file_name))
+        urllib.request.urlretrieve(line,os.path.join(dir,file_name))
+    except Exception as e:
+        print(e)
+        pass
+finally:
+    file_object.close()
