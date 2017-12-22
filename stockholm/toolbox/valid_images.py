9052561bce4ada256a8ae7ff1d6b67b024e954f5
puppyriver <4278246@qq.com>
2017-12-22 10:06:04 +0800

    123

--- stockholm/toolbox/valid_images.py
+++ stockholm/toolbox/valid_images.py
@@ -17,10 +17,14 @@ for parent, dirnames, filenames  in os.walk("download_images"):
         else:
             with open(file, "rb") as f:
                 bytes = f.read()
+                notImg = False
                 if not (bytes.startswith(b'\xff\xd8') or bytes.startswith(b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A')):
                     # bad image
                     print("invalid image %s" % file)
-                    shutil.copy(file, os.path.join(invalid_dest, image))
+                    notImg = True
+
+            if notImg:
+                shutil.move(file, os.path.join(invalid_dest, image))
 
 
 
