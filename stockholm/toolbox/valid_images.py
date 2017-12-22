d5488274b33fd1c262ce6a8d3db34902969516c4
puppyriver <4278246@qq.com>
2017-12-22 08:59:23 +0800

    1233

--- stockholm/toolbox/valid_images.py
+++ stockholm/toolbox/valid_images.py
@@ -1,11 +1,14 @@
 import imghdr
 import os
 
-for file in os.walk("download_images"):
-    if imghdr.what(file):
-        # good image
-        pass
-    else:
-        # bad image
-        print("invalid image %s" % file)
+for parent, dirnames, filenames  in os.walk("download_images"):
+    for image in filenames:
+        file = os.path.join(parent,image)
+        if imghdr.what(file):
+            # good image
+            pass
+        else:
+            # bad image
+            print("invalid image %s" % file)
+
 
