c03888e668406dc9b6e2fabec1983f9ebff35fcb
puppyriver <4278246@qq.com>
2018-02-07 19:05:47 +0800

    km

--- stockholm/toolbox/valid_images.py
+++ stockholm/toolbox/valid_images.py
@@ -11,21 +11,28 @@ if not os.path.exists(invalid_dest):
 for parent, dirnames, filenames  in os.walk("download_images"):
     for image in filenames:
         file = os.path.join(parent,image)
+        notImg = False
         if imghdr.what(file):
-            # good image
-            pass
+            size = os.path.getsize(file)
+            if size == 2051 or size < 1000:
+                notImg = True
+
         else:
             with open(file, "rb") as f:
                 bytes = f.read()
-                notImg = False
                 if not (bytes.startswith(b'\xff\xd8') or bytes.startswith(b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A')):
                     # bad image
                     print("invalid image %s" % file)
                     notImg = True
 
 
-            if notImg:
-                shutil.move(file, os.path.join(invalid_dest, image))
+        if notImg:
+            shutil.move(file, os.path.join(invalid_dest, image))
+
+        else:
+            if not (image.find(".") > 0):
+                shutil.move(file, os.path.join(parent,image+".jpg"))
+                print("change file name %s to %s" % (image,image+".jpg"))
 
 
 
