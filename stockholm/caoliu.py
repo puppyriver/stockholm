8335f6a9cb11481d1d4deaec0305335ba843517b
puppyriver <4278246@qq.com>
2018-02-23 21:47:03 +0800

    123

--- stockholm/caoliu.py
+++ stockholm/caoliu.py
@@ -14,25 +14,25 @@ import urllib.request
 if __name__ == '__main__':
     for page in range(1,200):
         url = "https://t66y.com/thread0806.php?fid=25&search=&page="+str(page);
-        # r = requests.get(url)
-        # if (r.status_code == 200):
-        #     text = r.text
-        #     text = str(text,"utf-8")
-        #     if page == 1:
-        #         print(text);
-        #     if text.find("rxrj") > 0:
-        #         print("find rxrj :"+str(page));
-        #     else:
-        #         print("not find : "+str(page));
-
-        f = urllib.request.urlopen(url)
-        data = f.readlines()
-        for l in data:
-            line = str(l, "utf-8").strip()
-            if page == 1:
-                print(line);
-            if line.find("rxrj") > 0:
-                print("find rxrj :" + str(page));
+        r = requests.get(url)
+        if (r.status_code == 200):
+            text = r.text
+            text = str(text,"utf-8")
+            if text.find("cdmindstorm") > 0:
+                print("find cdmindstorm - "+str(page))
+            if text.find("rxrj") > 0:
+                print("find rxrj :"+str(page));
             else:
                 print("not find : "+str(page));
+
+        # f = urllib.request.urlopen(url)
+        # data = f.readlines()
+        # for l in data:
+        #     line = str(l, "utf-8").strip()
+        #     if page == 1:
+        #         print(line);
+        #     if line.find("rxrj") > 0:
+        #         print("find rxrj :" + str(page));
+        #     else:
+        #         print("not find : "+str(page));
 
