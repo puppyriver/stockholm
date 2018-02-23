464d0d688f9373f1b5521e51e9b5e4b76929281c
puppyriver <4278246@qq.com>
2018-02-23 21:30:29 +0800

    11

--- stockholm/caoliu.py
+++ stockholm/caoliu.py
@@ -13,12 +13,12 @@ import requests
 if __name__ == '__main__':
     for page in range(1,200):
         r_params = {};
-        r = requests.get("https://t66y.com/thread0806.php?fid=25&search=&page="+page)
+        r = requests.get("https://t66y.com/thread0806.php?fid=25&search=&page="+str(page))
         if (r.status_code == 200):
             text = r.text
             if text.find("rxrj") > 0:
-                print("find rxrj :"+page);
+                print("find rxrj :"+str(page));
             else:
-                print("not find : "+page);  
+                print("not find : "+str(page));
 
 
